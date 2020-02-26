3e86252871b5506a93b0751b8f9f600c21917dbc
Dheeraj Kumar <54562389+dkumar1997@users.noreply.github.com>
2020-02-25 20:11:17 -0700

    Update Card.java

--- Card.java
+++ Card.java
@@ -2,9 +2,13 @@
 public class Card{
     private String suit;
     private int number;
+    private Card card;
     public Card(int number, String suit){
         this.number = number;
         this.suit = suit;
+    }
+    public Card(Card newCard){
+        this.card = newCard;
     }
     
     public int getcard(){
